1b671f9c85bdd8c2e83e41c38d5e32185a5c1620
Asonjay <jasonxu44@hotmail.com>
2021-03-22 14:29:31 -0400

    Update with async function
    To get rid of hard code part

--- app.js
+++ app.js
@@ -13,102 +13,21 @@ function Dinosaur(dino) {
     this.fact = dino.fact;
 }
 
-/** 
- * //TODO: Need to refactor this code to get rid of the hard-code part of JSON data.
- * @description The snippet of 'fetch' command
-``
-    let myInit = {
-        Dinos: []
-    };
-
-    let myRequest = new Request("dino.json", myInit);
-    //Credit to https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    fetch(myRequest)
-    .then(response => response.json())
-    .then(result => {
-        console.log(result)
-    })
-    .catch(error => {console.error("Failed")});
-``
-*/
-
 /**
+ * ## Modified based on review ##
  * @description function to return json data
  * @returns Array of dinosaur data from dino.json
  */
  function dinoData(){
-    return [{
-                "species": "Triceratops",
-                "weight": 13000,
-                "height": 114,
-                "diet": "herbavor",
-                "where": "North America",
-                "when": "Late Cretaceous",
-                "fact": "First discovered in 1889 by Othniel Charles Marsh"
-            },
-            {
-                "species": "Tyrannosaurus Rex",
-                "weight": 11905,
-                "height": 144,
-                "diet": "carnivor",
-                "where": "North America",
-                "when": "Late Cretaceous",
-                "fact": "The largest known skull measures in at 5 feet long."
-            },
-            {
-                "species": "Anklyosaurus",
-                "weight": 10500,
-                "height": 55,
-                "diet": "herbavor",
-                "where": "North America",
-                "when": "Late Cretaceous",
-                "fact": "Anklyosaurus survived for approximately 135 million years."
-            },
-            {
-                "species": "Brachiosaurus",
-                "weight": 70000,
-                "height": "372",
-                "diet": "herbavor",
-                "where": "North America",
-                "when": "Late Jurasic",
-                "fact": "An asteroid was named 9954 Brachiosaurus in 1991."
-            },
-            {
-                "species": "Stegosaurus",
-                "weight": 11600,
-                "height": 79,
-                "diet": "herbavor",
-                "where": "North America, Europe, Asia",
-                "when": "Late Jurasic to Early Cretaceous",
-                "fact": "The Stegosaurus had between 17 and 22 seperate places and flat spines."
-            },
-            {
-                "species": "Elasmosaurus",
-                "weight": 16000,
-                "height": 59,
-                "diet": "carnivor",
-                "where": "North America",
-                "when": "Late Cretaceous",
-                "fact": "Elasmosaurus was a marine reptile first discovered in Kansas."
-            },
-            {
-                "species": "Pteranodon",
-                "weight": 44,
-                "height": 20,
-                "diet": "carnivor",
-                "where": "North America",
-                "when": "Late Cretaceous",
-                "fact": "Actually a flying reptile, the Pteranodon is not a dinosaur."
-            },
-            {
-                "species": "Pigeon",
-                "weight": 0.5,
-                "height": 9,
-                "diet": "herbavor",
-                "where": "World Wide",
-                "when": "Holocene",
-                "fact": "All birds are living dinosaurs."
-            }]
+    return fetch("dino.json")
+    .then((res) => res.json())
+    .then((res) => {
+        console.log("JSON data", res);
+        return res;
+    })
+    .catch((error) => {
+        console.error("Failed");
+    });
 }
 
 /**
@@ -272,7 +191,6 @@ function generateHumanTile(humanData){
         if (i == 4){
             generatedDiv.appendChild(generateHumanTile(humanData));
         }else{
-            console.log(dinoObjs[counter]);
             generatedDiv.appendChild(generateDinoTile(dinoObjs[counter], humanData));
             counter++;
         }
@@ -285,10 +203,11 @@ function generateHumanTile(humanData){
  * @description actions when "compare me" being clicked
  * @updates index.html
  */
- function click(){
+ async function click(){
     // generate data
     const humanData = getHumanData();
-    const dinoObjs = createDinoObjs(dinoData());
+    const jsonData = await dinoData();
+    const dinoObjs = createDinoObjs(jsonData.Dinos);
     shuffle(dinoObjs);
     // Hide input data form
     document.getElementById("dino-compare").style.display = "none";
